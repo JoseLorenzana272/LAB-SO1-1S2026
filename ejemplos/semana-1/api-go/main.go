cfa141fa58edae72e931954add601142cab9fdf8
roldyoran <edgaralvares4208@gmail.com>
2026-01-31 19:54:48 -0600

    add: ejemplos semana 1

---
+++ ejemplos/semana-1/api-go/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "log"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+    app := fiber.New()
+
+    app.Use(logger.New())
+
+    app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Bienvenidos")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+}
