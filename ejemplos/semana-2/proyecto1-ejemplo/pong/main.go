137e3bb15959809a736f21f27b4872b2c2110bd9
roldyoran <edgaralvares4208@gmail.com>
2026-02-02 19:36:55 -0600

    Add: ping and pong example services

---
+++ ejemplos/semana-2/proyecto1-ejemplo/pong/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	// Middleware de logger para ver las peticiones
+	app.Use(logger.New())
+
+	// Endpoint /responder migrado a Fiber
+	app.Get("/responder", func(c *fiber.Ctx) error {
+		return c.SendString("¡Pong! (Desde Containerd en VM 2)")
+	})
+
+	// Endpoint de salud similar al ejemplo de semana 2
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(map[string]string{"status": "UP", "message": "Pong API is Ready"})
+	})
+
+	fmt.Println("API PONG corriendo en puerto 8082")
+	log.Fatal(app.Listen(":8082"))
+}
